ec7f5bb4cfcf7dc5fc4be2a5c5b48d68a47e9008
JuncWang <juncwang@live.com>
2018-05-23 17:32:54 +0800

    change

--- shopping.py
+++ shopping.py
@@ -2,27 +2,30 @@
 #date: 2018/5/23
 
 salary = int(input("请输入你的资金 :\t"))
-commodity = [ 'iphone6s','macbook','coffee','pythonbook','bicyle']
-price = [5800,9000,32,80,1500]
 
-PurCom = []
-PurPri = []
-PurInt = []
-PurNum = 0
+product_list = [
+    ('iphone6s',5800),
+    ('macbook',9000),
+    ('coffee',32),
+    ('pythonbook',80),
+    ('bicyle',1500)
+]
 
+buy_list = []
 
 while True:
     print("请选择你想购入的物品 :\t")
 
-    for i in range(5):
-        print("%s.\t%s\t%s" % (i+1,commodity[i],price[i]))
+    for i,v in enumerate(product_list,1):
+        print("%s.---------------------\t%s\t%s" % (i,v[0],v[1]))
 
-    num = input("请输入你想购买商品的序号 :\t")
+    num = input("请输入你想购买商品的序号[exit:q] :\t")
 
-    if num == "quit":
-        print("你所购买的物品有 :")
-        for j in range(PurNum):
-            print("%s.\t%s\t%s\t%s\t%s" % (j+1,PurCom[j],PurPri[j],PurInt[j],PurInt[j]*PurPri[j]))
+    if num == "q":
+        if len(buy_list) != 0:
+            print("你所购买的物品有 :")
+            for j,l in enumerate(buy_list,1):
+                print("%s.---------------------\t%s\t%s" % (j,l[0],l[1]))
 
         print("当前余额 :\t%s" %salary)
         break
@@ -31,21 +34,14 @@ while True:
         num = int(num)-1
 
         if 0 <= num < 5:
-
-            if price[num] <= salary:
-                salary -= price[num]
-                print("已加入 %s 到你的购物车\t当前余额: %s" %(commodity[num],salary))
-
-                if PurCom.count(commodity[num]) == 0:
-                    PurCom.append(commodity[num])
-                    PurPri.append(price[num])
-                    PurInt.append(1)
-                    PurNum += 1
-                else:
-                    PurInt[PurCom.index(commodity[num])] += 1
+            p_item = product_list[num]
+            if p_item[1] <= salary:
+                salary -= p_item[1]
+                print("已加入 %s 到你的购物车\t当前余额: %s" %(p_item[0],salary))
+                buy_list.append((p_item[0],p_item[1]))
 
             else:
-                print("余额不足\t%s" %(salary - price[num]))
+                print("余额不足\t%s" %(salary - p_item[1]))
 
         else:
             print("你输入的商品有误!")
